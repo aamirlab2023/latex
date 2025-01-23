52baba30c1c1b7ca9ef00f9b18e75d6504abf67c
aamirlab2023 <136485053+aamirlab2023@users.noreply.github.com>
2025-01-23 08:20:56 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,8 @@ sudo apt-get install -y texlive-base
 sudo apt-get install -y texlive-latex-recommended
 sudo apt-get install -y texlive
 sudo apt-get install -y texlive-latex-extra
+sudo apt-get install -y texlive-fonts-recommended
+sudo apt-get install -y texlive-fonts-extra
+sudo apt-get install -y textlive-full
+sudo apt-get install -y xetex
+sudo apt-get install -y lualatex
